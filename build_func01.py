032c40715e4bd3c5a9f179f053173231eb0df859
nurimova <n.b.sitora@gmail.com>
2024-12-07 23:29:37 +0500

    function

--- build_func01.py
+++ build_func01.py
@@ -1,12 +1,3 @@
-def main(number):
-    """Interger turdagi butun sonli o'zgaruvchi 'number' berilgan. 'number' ning mutlaq qiymatini qaytarish.
-
-    Argümentlar:
-        number (int): butun son
-
-    Qaytish:
-        int: 'number' ning mutlaq qiymati
-    """
-    return number
-s=main(15)
+n=-8
+s=abs(n)
 print(s)
